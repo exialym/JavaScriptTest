41a1b057c9e06b609803ce2c39adbcb974129648
exialym <exialym@icloud.com>
2016-05-09 23:22:52 +0800

    12.1.2 DOM杂七杂八的变化

--- test12_.js
+++ test12_.js
@@ -13,20 +13,21 @@
 // alert(supportsDOM2Views);
 // alert(supportsDOM2XML);
 /*********************************与XHTML命名空间有关的属性************/
-//DOM2
-var html = document.documentElement;
-alert(html.localName);
-alert(html.namespaceURI);
-alert(html.prefix);
-var svg = document.getElementsByTagName("svg")[0];
-alert(html.isDefaultNamespace("http://www.w3.org/1999/xhtml"));
-alert(document.body.isDefaultNamespace("http://www.w3.org/1999/xhtml"));
-alert(svg.localName);
-alert(svg.namespaceURI);
-alert(svg.prefix);
-//这里比较有趣，svg节点的defaultNameSpace不是我们给他设置的那个而是上一级的。
-alert(svg.isDefaultNamespace("http://www.w3.org/1999/xhtml")); //true
-alert(svg.isDefaultNamespace("http://www.w3.org/2000/svg")); //false
-alert(svg.lookupNamespaceURI("s"));
-alert(svg.lookupPrefix("http://www.w3.org/2000/svg"));
-
+// //DOM2
+// var html = document.documentElement;
+// alert(html.localName);
+// alert(html.namespaceURI);
+// alert(html.prefix);
+// var svg = document.getElementsByTagName("svg")[0];
+// alert(html.isDefaultNamespace("http://www.w3.org/1999/xhtml"));
+// alert(document.body.isDefaultNamespace("http://www.w3.org/1999/xhtml"));
+// alert(svg.localName);
+// alert(svg.namespaceURI);
+// alert(svg.prefix);
+// //这里比较有趣，svg节点的defaultNameSpace不是我们给他设置的那个而是上一级的。
+// alert(svg.isDefaultNamespace("http://www.w3.org/1999/xhtml")); //true
+// alert(svg.isDefaultNamespace("http://www.w3.org/2000/svg")); //false
+// alert(svg.lookupNamespaceURI("s"));
+// alert(svg.lookupPrefix("http://www.w3.org/2000/svg"));
+/*********************************defaultView属性,代表拥有此文档的窗口或框架***/
+alert(document.defaultView || document.parentWindow);
